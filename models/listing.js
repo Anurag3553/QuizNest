92580615952c24561f1bb458d2624a6c9f9fa4e4
Anurag Yadav <164323508+Anurag3553@users.noreply.github.com>
2025-08-26 11:53:07 +0530

    Add files via upload

--- models/listing.js
+++ models/listing.js
@@ -1,1 +1,50 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
+const quizSchema = new Schema({
+  quizName: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  points: {
+    type: Number,
+    required: true,
+  },
+  timeLimit: Number, // in minutes or seconds
+  noOfQuestions: Number,
+  description: String,
+
+  // Embedding questions
+  questions: [
+    {
+      questionText: {
+        type: String,
+        required: true,
+      },
+      options: [
+        {
+          text: {
+            type: String,
+            required: true,
+          },
+          isCorrect: {
+            type: Boolean,
+            required: true,
+          },
+        },
+      ],
+    }
+  ],
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const CreateQuiz = new mongoose.model("CreateQuiz", quizSchema);
+module.exports = CreateQuiz;
